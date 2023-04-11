8df19c2afb42eac2bde99573adb93c45da5174f7
E. Hannah <echannah631@gmail.com>
2023-04-11 11:57:40 -0500

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Elliot Hannah III on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
